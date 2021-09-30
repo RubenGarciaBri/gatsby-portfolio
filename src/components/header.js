74cdcb62e6c83d865352f02e02816124bbfcdd24
Ruben <hello@rubengarciabri.me>
2021-09-30 15:36:31 +0200

    fix(Added header)

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,83 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
+import * as React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <nav className="navbar py-3 z-50">
+    <div className="main-container flex items-center justify-between">
+      <div className="logo-box">
+        <a href="#section-1" className="">
+          <FaCode className="mr-40 text-6xl" />
+        </a>
+      </div>
+      <ul className="flex items-center justify-center hidden sm:flex">
+        <li className="">
+          <a
+            href="#section-2"
+            className="mr-4 py-2 px-2 font-bold text-xl tracking-wide"
+          >
+            About
+          </a>
+        </li>
+        <li className="navbar__list-item">
+          <a
+            href="#section-3"
+            className="mr-4 py-2 px-2 font-bold text-xl tracking-wide"
+          >
+            Certifications
+          </a>
+        </li>
+        <li className="navbar__list-item">
+          <a
+            href="#section-4"
+            className="mr-4 py-2 px-2 font-bold text-xl tracking-wide"
+          >
+            Portfolio
+          </a>
+        </li>
+        <li className="navbar__list-item">
+          <a
+            href="#section-5"
+            className="py-2 px-2 font-bold text-xl tracking-wide"
+          >
+            Contact
+          </a>
+        </li>
+      </ul>
+      {/* Mobile Menu */}
+      <FaBars className="sideNav__icon sm:hidden" />
+      <ul className="sideNav shadow-slim sm:hidden">
+        <li className="sideNav__item">
+          <a href="#section-2">
+            <span>About</span>
+          </a>
+        </li>
+        <li className="sideNav__item">
+          <a href="#section-3">
+            <span>Certifications</span>
+          </a>
+        </li>
+        <li className="sideNav__item">
+          <a href="#section-4">
+            <span>Portfolio</span>
+          </a>
+        </li>
+        <li className="sideNav__item">
+          <a href="#section-5">
+            <span>Contact</span>
+          </a>
+        </li>
+      </ul>
     </div>
-  </header>
-)
+  </nav>
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
