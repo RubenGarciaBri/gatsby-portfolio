02a2ccae445b0d12e6fb12c367ea745c36d356d2
rubengarciabri <ruben@behaviourlab.com>
2022-01-06 19:36:25 +0100

    feat(refactoring): Added dynamically generated pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ graphql, actions }) => {
+  const { data } = await graphql(`
+    query GetProjects {
+      allMdx {
+        edges {
+          node {
+            frontmatter {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  data.allMdx.edges.forEach(edge => {
+    const slug = edge.node.frontmatter.slug;
+    actions.createPage({
+      path: `/projects/${slug}`,
+      component: path.resolve("./src/templates/project-details.tsx"),
+      context: { slug: slug },
+    });
+  });
+};
---
+++ src/templates/project-details.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { graphql } from "gatsby";
+import Header from "../components/header";
+
+const ProjectDetails = ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export const query = graphql`
+  query getProject($slug: String) {
+    mdx(frontmatter: { slug: { eq: $slug } }) {
+      frontmatter {
+        date
+        excerpt
+        featureImage
+        slug
+        title
+      }
+    }
+  }
+`;
+
+export default ProjectDetails;
