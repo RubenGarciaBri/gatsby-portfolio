413aca5d31492b47836a53fe47f33ee20c21a0c4
rubengarciabri <ruben@behaviourlab.com>
2022-01-15 18:10:06 +0000

    feat(refactoring): Made hero responsive

--- src/components/HeroContainer/HeroContainer.tsx
+++ src/components/HeroContainer/HeroContainer.tsx
@@ -14,16 +14,16 @@ interface IPropTypes {
 const HeroContainer = ({ heading, subHeading }: IPropTypes) => {
   return (
     <div className="main-container">
-      <div className="flex flex-col items-stretch mt-32 text-center lg:text-left mb-52 justify-items-start lg:flex-row">
-        <div className="mr-8 lg:w-3/5">
-          <div className="flex items-center justify-center mb-6 lg:justify-start gap-x-5">
+      <div className="flex flex-col items-stretch mt-32 text-center xl:text-left mb-52 justify-items-start xl:flex-row">
+        <div className="mr-8 lg:mx-auto lg:w-4/5 xl:w-3/5">
+          <div className="flex items-center justify-center mb-6 xl:justify-start gap-x-5">
             <h3 className="flex text-xl font-semibold text-gray-400 sm:text-2xl subheading">
               {subHeading}
             </h3>
             <StaticImage
               placeholder="blurred"
               src="../../images/prof-small.png"
-              className="w-12 w-full bg-gray-200 rounded-full shadow-inner md:w-14 lg:hidden"
+              className="w-12 w-full bg-gray-200 rounded-full shadow-inner md:w-14 xl:hidden"
               alt="Ruben's profile picture"
             />
           </div>
@@ -31,17 +31,17 @@ const HeroContainer = ({ heading, subHeading }: IPropTypes) => {
             A <span className="font-bold text-blue-500">React Developer</span>{" "}
             with passion for the new web technologies
           </h1>
-          <ul className="flex justify-center mb-12 lg:justify-start">
+          <ul className="flex justify-center mb-12 xl:justify-start">
             {socialLinksData &&
               socialLinksData.map(({ type, url }) => {
                 return (
-                  <li className="mr-4">
+                  <li key={type} className="mr-4">
                     <SocialButton type={type} url={url} />
                   </li>
                 );
               })}
           </ul>
-          <div className="flex justify-center mt-1 gap-x-6 lg:justify-start">
+          <div className="flex justify-center mt-1 gap-x-6 xl:justify-start">
             <PrimaryButton>Let's Talk</PrimaryButton>
             <SecondaryButton>See My Work</SecondaryButton>
           </div>
@@ -50,7 +50,7 @@ const HeroContainer = ({ heading, subHeading }: IPropTypes) => {
           <StaticImage
             placeholder="blurred"
             src="../../images/side-2.png"
-            className="hidden w-full bg-gray-200 rounded-full shadow-inner lg:block profile-img"
+            className="hidden w-full bg-gray-200 rounded-full shadow-inner xl:block profile-img"
             alt="Ruben's profile picture"
           />
         </div>
