c912e8aadb47ab459cd659aa7b7cebb79ec6ff11
rubengarciabri <ruben@behaviourlab.com>
2022-02-20 19:07:53 +0000

    feat(refactoring): Refactored blog article template

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { MDXProvider } from "@mdx-js/react";
+import { StaticImage } from "gatsby-plugin-image";
 
 import { badges } from "../data/badges";
 import { MDXComponents } from "../components/MDX/MDXComponents";
@@ -19,23 +20,29 @@ const Article = ({ data }) => {
   return (
     <MDXProvider components={MDXComponents}>
       <Layout>
-        <div className="main-container">
-          <div className="py-32 my-12 bg-gray-200 rounded-xl">
-            <div className="text-center">
-              <h1 className="mb-4 text-5xl font-bold">{title}</h1>
-              <span>{author} - </span>
-              <span>{date}</span>
-              {/* <Img fluid={featuredImg.childImageSharp.fluid} /> */}
+        <div className="container-sm">
+          <div className="my-32 text-center">
+            <h1 className="mb-6 text-5xl font-bold">{title}</h1>
 
-              <ul>
-                {badgeNames &&
-                  badgeNames.map((badgeName, index) => {
-                    return (
-                      <li key={index + badgeName}>{badges[badgeNames]}</li>
-                    );
-                  })}
-              </ul>
+            <div className="flex items-center justify-center gap-3 mb-2">
+              <StaticImage
+                src="../images/prof-small.png"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="Author's profile picture"
+              />
+              <div>
+                <span className="font-bold">{author}</span> | {date}
+              </div>
             </div>
+
+            <ul>
+              {badgeNames &&
+                badgeNames.map((badgeName, index) => {
+                  return <li key={index + badgeName}>{badges[badgeNames]}</li>;
+                })}
+            </ul>
           </div>
           <div className="container-sm">
             {<MDXRenderer>{body}</MDXRenderer>}
